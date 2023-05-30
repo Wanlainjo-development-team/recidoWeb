e7bf428e6ee5b17edb6439b3cd64fa838176f7a1
romaric <41764271+ngehromaric@users.noreply.github.com>
2023-05-30 20:31:56 +0100

    Added firebase configuation with rockkiecodes aka terry

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,46 +1,62 @@
 
 <script >
-
-export default{
-  data:()=>({  
+export default {
+  data: () => ({
     companyNmae: "WANLAINJO COMPUTERS",
     drawerItem: [
-      {title:'Iventory', path:'/inventory/productlist',icon:'mdi-StoreOutline',color:'info'},
-      {title:'Sales', path:'/sales/orderlist',icon:'mdi-shopping',color:'primary'},
-      {title:'Report', path:'/report/salesreport',icon:'mdi-shopping',color:'yellow'},
-      {title:'Customers', path:'/customers/customerlist',icon:'mdi-user',color:'green'},
+      {
+        title: "Dashboard",
+        path: "/",
+        icon: "mdi-view-dashboard",
+        color: "info",
+      },
+      {
+        title: "Iventory",
+        path: "/inventory/productlist",
+        icon: "mdi-FormatListBulleted",
+        color: "info",
+      },
+      {
+        title: "Sales",
+        path: "/sales/orderlist",
+        icon: "mdi-shopping",
+        color: "primary",
+      },
+      {
+        title: "Report",
+        path: "/report/salesreport",
+        icon: "mdi-shopping",
+        color: "yellow",
+      },
+      {
+        title: "Customers",
+        path: "/customers/customerlist",
+        icon: "mdi-account-group",
+        color: "green",
+      },
     ],
-  })
-}
-
+  }),
+};
 </script>
 
 <template>
-  <v-app-bar >
+  <v-app-bar>
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-6" />
-       {{ companyNmae }}
+      {{ companyNmae }}
     </v-app-bar-title>
   </v-app-bar>
-   
-  <v-navigation-drawer
-  permanent
-  location="left"
-  >
-   <v-list>
-      <v-list-item> 
-        <v-btn v-for="(route, i) in drawerItem" :key="i" :to="route.path"
-        color= route.color
-        min-width="200"
-        size="large"
-        variant="flat"
-        elevated
-      >
-         {{ route.title }}
-      </v-btn>
-      <v-divider/>
+
+  <v-navigation-drawer permanent location="left">
+    <v-list>
+      <v-list-item v-for="(route, i) in drawerItem" :key="i" :to="route.path">
+        <div :class="`bg-${route.color}`" class="rounded-lg py-3 px-2">
+          <v-icon :icon="route.icon"></v-icon>
+          {{ route.title }}
+        </div>
+        <v-divider />
       </v-list-item>
-   </v-list>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
