cf0f2b10b0d827b4fedbf9e3d269cddc7491782e
romaric <41764271+ngehromaric@users.noreply.github.com>
2023-05-30 14:39:45 +0100

    working on routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 // Composables
+import PurchaseReportVue from '@/components/ProductsManagement/Reports/PurchaseReport.vue'
+import SalesReportVue from '@/components/ProductsManagement/Reports/SalesReport.vue'
+import StockLevelVue from '@/components/ProductsManagement/Reports/StockLevel.vue'
 import OrderListVue from '@/components/ProductsManagement/Sales/OrderList.vue'
+import SaleQuoteVue from '@/components/ProductsManagement/Sales/SaleQuote.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -12,13 +16,45 @@ const routes = [
         name: 'Home',
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path:'/sales/orderlist',
+        name: 'OrderList',
+        component: OrderListVue
+     },
+     {
+          path:'/sales/salesreport',
+          name: 'SalesReport',
+          component: SalesReportVue
+    },
+     {
+          path:'/sales/salesquote',
+          name: 'SalesQuote',
+          component: SaleQuoteVue
+    },
+     {
+          path:'/report/purchasereport',
+          name: 'purchasereport',
+          component: PurchaseReportVue
+    },
+     {
+          path:'/report/salesreport',
+          name: 'salereport',
+          component: SalesReportVue
+    },
+     {
+          path:'/report/salesreport',
+          name: 'salereport',
+          component: SalesReportVue
+    },
+     {
+          path:'/report/stocklevel',
+          name: 'stocklevel',
+          component: StockLevelVue
+    },
     ],
   },
-  {
-    path:'/sales/orderlist',
-    name: 'OrderList',
-    component: OrderListVue
-  }
+  
+  
 ]
 
 const router = createRouter({
