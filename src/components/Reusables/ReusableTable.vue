adb87fc4fca812bd583bada0189964e08e426f6d
romaric <41764271+ngehromaric@users.noreply.github.com>
2023-06-01 05:42:18 +0100

    Craeting static search bars

--- src/components/Reusables/ReusableTable.vue
+++ src/components/Reusables/ReusableTable.vue
@@ -9,7 +9,7 @@ export default {
     }
   },
     data: ()=>({  
-        tableTile: "Inventory9",
+        tableTile: "Inventory93",
        
     })
 }
--- src/components/Reusables/SearchBar.vue
+++ src/components/Reusables/SearchBar.vue
@@ -2,7 +2,7 @@
 export default {
  
      
-    name: "Productlist",
+    name: "searchbar",
     data: () => ({
       loaded: false,
       loading: false,
@@ -26,14 +26,14 @@ export default {
         <div>
         <v-sheet class="d-flex flex-wrap " elevation="2">
             <v-sheet class="flex-1-0 ma-2 pa-2">
-               <h1>Products</h1>
+               <h1>Invoices</h1>
             </v-sheet>
             <v-sheet class="flex-1-0 ma-2 pa-2">
                 <v-text-field
                     :loading="loading"
                     density="compact"
                     variant="solo"
-                    label="Search for Products"
+                    label="Search Invoice"
                     append-inner-icon="mdi-magnify"
                     single-line
                     hide-details
@@ -43,7 +43,7 @@ export default {
           
              
             <v-sheet class="ma-2 pa-2 ">
-               <v-btn class="bg-primary">New Products</v-btn>
+               <v-btn class="bg-primary">New Invoice</v-btn>
             </v-sheet>
 
           </v-sheet>
