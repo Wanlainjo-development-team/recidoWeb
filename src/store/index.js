29709ad4cb10d12826b9c6705640dced7ac166ec
romaric <41764271+ngehromaric@users.noreply.github.com>
2023-06-07 01:39:56 +0100

    pulling invoice from data base logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,27 @@
 // Utilities
 import { createPinia } from 'pinia'
+import { db } from '@/plugins/firebase'
 
-export default createPinia()
+export default createPinia({
+    state:{
+        invoiceData:[]
+    },
+    actions: {
+        async GET_INVOICES({commit,state}){
+            const getData = db.collection('invoices');
+            const results = await getData.get();
+            //ietterating through the results
+            results.forEach(doc => {
+                //if invocie already exist in the invocieDat it wont be added twice
+                if(!state.invoiceData.some(invocie => invocie.docId === doc.id)){
+                    const data ={
+                        docId: doc.id,
+                        state:  doc.data().state,
+                        address: doc.data().address,
+                    };
+                }
+            })
+
+        }
+    },
+})
