087f21b57051bde629e60fcb67f04b12741dce48
romaric <41764271+ngehromaric@users.noreply.github.com>
2023-06-17 16:43:36 +0100

    m

--- src/components/ProductsManagement/Customers/CustomerList.vue
+++ src/components/ProductsManagement/Customers/CustomerList.vue
@@ -1,5 +1,6 @@
 <script>
 import ReusableTableHeader from "@/components/Reusables/ReusableTableHeader.vue";
+import { useCustomerStore } from "@/store/customers";
 export default {
   components: { ReusableTableHeader },
 
@@ -8,8 +9,15 @@ export default {
     loaded: false,
     loading: false,
   }),
+
+  setup() {
+    const customers = useCustomerStore();
+
+    return { customers };
+  },
 };
 </script>
+
 <template>
   <v-container>
     <div>
@@ -21,31 +29,37 @@ export default {
     <v-table>
       <thead>
         <tr>
-          <th class="text-left ">S/N</th>
-          <th class="text-left ">Invoice #</th>
-          <th class="text-left">Customer</th>
-          <th class="text-left">Date</th>
-          <th class="text-left">Items Count</th>
-          <th class="text-left">status</th>
+          <th class="text-left">S/N</th>
+          <th class="text-left">Name</th>
+          <th class="text-left">Number</th>
+          <th class="text-left">Customer Rating</th>
+          <th class="text-left">Location</th>
           <th class="text-left">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="customer in customers.customerList" :key="customer.id">
-          <td > {{count ++}}</td>
+        <tr
+          v-for="(customer, count) in customers.customerList"
+          :key="customer.id"
+        >
+          <td>{{ count }}</td>
           <td>{{ customer.name }}</td>
-           
+          <td>{{ customer.phoneNumbers[0].digits }}</td>
+          <td>{{ customer.location}}</td>
+          <td>{{ customer.city }}</td>
+
           <td>
-              <v-btn class="ma-1"
+            <v-btn
+              class="ma-1"
               prependIcon="mdi-pencil"
               color="success"
               variant="outlined"
-              >
-              Upate</v-btn>
-              <v-btn class="ma-1"
-              color="primary"
-              variant="outlined"
-              >View Details</v-btn>
+            >
+              Upate</v-btn
+            >
+            <v-btn class="ma-1" color="primary" variant="outlined"
+              >View Details</v-btn
+            >
           </td>
         </tr>
       </tbody>
---
+++ src/components/ProductsManagement/Sales/CreateNewInvoice.vue
@@ -0,0 +1,63 @@
+<script>
+import CustomerListVue from '../Customers/CustomerList.vue';
+export default {
+  name: "craeteNewInvocie",
+  data:()=>{ 
+     
+   },
+
+   methods: {
+     
+   },
+};
+</script>
+
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="5">
+          <v-row>
+            <v-col>
+                <v-text-field label="Bill to:" placeholder="Select Customer"  variant="underlined"></v-text-field>
+                <v-autocomplete
+                ref="customerList"
+                label="Select Customer"
+                placeholder="Select..."
+                required
+              ></v-autocomplete>
+            </v-col>
+            <v-col>
+                <v-btn>Add</v-btn>
+            </v-col>
+          </v-row>
+           
+        </v-col>
+ 
+        <v-col cols="12" md="2">
+           <v-input aria-placeholder="input" ></v-input>
+        </v-col>
+        <v-col cols="12" md="2">
+          col1
+        </v-col>
+        <v-col cols="12" md="2">
+          col1
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field label="First name" required></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field label="Last name" required></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field label="E-mail" required></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<style></style>
