7902baa285a56ea23ca87974bbe177d44df38f29
romaric <41764271+ngehromaric@users.noreply.github.com>
2023-06-18 22:53:33 +0100

    add new customer

--- src/store/customers.js
+++ src/store/customers.js
@@ -1,10 +1,27 @@
-import { db } from "@/plugins/firebase";
-import { collection, onSnapshot } from "firebase/firestore";
 import { defineStore } from "pinia";
+import { db } from "@/plugins/firebase";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+
+import { useAppStore } from "./app";
+
+const app = useAppStore;
 
 export const useCustomerStore = defineStore("customers", {
   state: () => ({
     customerList: [],
+    dialog: false,
+    customerName: "",
+    address: "",
+    contact: "",
+    email: "",
+    loading: false,
   }),
 
   actions: {
@@ -23,6 +40,42 @@ export const useCustomerStore = defineStore("customers", {
       });
 
       return unsubscribe;
+    },
+
+    async NEW_CUSTOMER() {
+      let id = import.meta.env.VITE_UserId;
+
+      const customers = await getDocs(
+        query(
+          collection(db, "users", id, "customers"),
+          where("name", "==", this.customerName)
+        )
+      );
+
+      if (customers.docs.length >= 1) {
+        app().snackbar = true;
+        app().snackbarText = "This customer already exists";
+        app().snackbarColor = "red";
+      } else {
+        this.loading = true;
+
+        await addDoc(collection(db, "users", id, "customers"), {
+          name: this.customerName,
+          address: this.address,
+          phone: this.contact,
+          email: this.email,
+        });
+
+        app().snackbar = true;
+        app().snackbarText = "Customer added successfully";
+        app().snackbarColor = "green";
+
+        this.loading = false;
+        this.dialog = false;g
+      }
+      // const newUser = collection(db, "users" , id, "customers").set({
+      //     name:
+      // })
     },
   },
 });
