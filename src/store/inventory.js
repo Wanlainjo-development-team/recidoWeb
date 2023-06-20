f26d9929ade5809fe84b1c822dfdae4170952d74
romaric <41764271+ngehromaric@users.noreply.github.com>
2023-06-20 23:09:54 +0100

    calling inventorylist from firebase

---
+++ src/store/inventory.js
@@ -0,0 +1,26 @@
+import { db } from "@/plugins/firebase";
+import { collection, onSnapshot } from "firebase/firestore";
+import { defineStore } from "pinia";
+
+export const useInventory = defineStore("inventory",{
+    sate:() =>({
+        inventoryList: [],
+    }),
+    actions:{
+       async GET_INVENTORY(){
+        let id = import.meta.env.VITE_UserId;
+
+        const i = collection(db, "users",id,"inventory");
+        const unsubscribe = onSnapshot(i,(querySnapshot) =>{
+            this.inventoryList = [];
+            querySnapshot.forEach((doc) =>{
+                this.inventoryList.push({
+                    id: doc.id,
+                    ...doc.data(),
+                });
+            });
+        });
+        return unsubscribe
+       }
+    },
+})
